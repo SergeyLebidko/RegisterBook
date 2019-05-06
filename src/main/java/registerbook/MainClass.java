2abf2e7ef598c37b4bdd5bb26199973043163c59
SergeyLebidko <it.sergeyler@mail.ru>
2019-05-06 18:55:16 +0300

    Созданы ссылки на графические ресурсы и на класс работы с БД

--- src/main/java/registerbook/MainClass.java
+++ src/main/java/registerbook/MainClass.java
@@ -1,9 +1,26 @@
 package registerbook;
 
+import registerbook.data_access_components.DBHandler;
+
+import javax.swing.*;
+
 public class MainClass {
 
+    private static DBHandler dbHandler;
+
     public static void main(String[] args) {
-        System.out.println("Привет");
+
+        try {
+            dbHandler=new DBHandler();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "", "", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+    }
+
+    public static DBHandler getDbHandler() {
+        return dbHandler;
     }
 
 }
--- src/main/java/registerbook/ResourcesList.java
+++ src/main/java/registerbook/ResourcesList.java
@@ -1,11 +1,57 @@
 package registerbook;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class ResourcesList {
 
-
-
+    //Параметры подключения к базе данных
     public static final String jdbcClassName = "org.sqlite.JDBC";
     public static final String databaseConnectionString = "jdbc:sqlite:database\\database.db";
 
+    //Графические ресурсы
+    private static ImageLoader imageLoader = new ImageLoader();
+    public static final Image logoImage = imageLoader.getImage("logo");
+    public static final ImageIcon addImage = imageLoader.getImageIcon("add");
+    public static final ImageIcon removeImage = imageLoader.getImageIcon("remove");
+
+    private static class ImageLoader{
+
+        private static final String[] imageNamesList = {
+                "logo",
+                "add",
+                "remove"
+        };
+
+        private HashMap<String, Image> imageMap = new HashMap<>();
+
+        public ImageLoader() {
+            ClassLoader classLoader = getClass().getClassLoader();
+            Image image;
+            for (String name: imageNamesList){
+                try {
+                    image = ImageIO.read(classLoader.getResourceAsStream("images/"+name+".png"));
+                } catch (IOException e) {
+                    image=null;
+                }
+                imageMap.put(name, image);
+            }
+        }
+
+        public Image getImage(String name){
+            return imageMap.get(name);
+        }
+
+        public ImageIcon getImageIcon(String name){
+            Image image;
+            image = imageMap.get(name);
+            if (image==null)return null;
+            return new ImageIcon(image);
+        }
+
+    }
 
 }
