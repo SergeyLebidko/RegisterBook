7713e6317ef6c06de9b40ac025378c1021c05e14
SergeyLebidko <it.sergeyler@mail.ru>
2019-05-06 18:12:49 +0300

    DBHandler теперь создает подключение к БД

---
+++ src/main/java/registerbook/ResourcesList.java
@@ -0,0 +1,9 @@
+package registerbook;
+
+public class ResourcesList {
+
+    public static final String jdbcClassName = "org.sqlite.JDBC";
+    public static final String databaseConnectionString = "jdbc:sqlite:database\\database.db";
+
+
+}
--- src/main/java/registerbook/data_access_components/DBHandler.java
+++ src/main/java/registerbook/data_access_components/DBHandler.java
@@ -1,11 +1,19 @@
 package registerbook.data_access_components;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import static registerbook.ResourcesList.*;
+
 public class DBHandler {
 
-    public DBHandler() {
+    private Statement statement;
 
-
-
+    public DBHandler() throws Exception {
+        Class.forName(jdbcClassName);
+        Connection connection = DriverManager.getConnection(databaseConnectionString);
+        statement = connection.createStatement();
     }
 
 }
