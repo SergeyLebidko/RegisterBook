0d5c15c8b01f05fcfd04657e8612396dc5b93960
SergeyLebidko <it.sergeyler@mail.ru>
2019-05-06 18:19:53 +0300

    Добавлен набор иконок

--- src/main/java/registerbook/ResourcesList.java
+++ src/main/java/registerbook/ResourcesList.java
@@ -2,6 +2,8 @@ package registerbook;
 
 public class ResourcesList {
 
+
+
     public static final String jdbcClassName = "org.sqlite.JDBC";
     public static final String databaseConnectionString = "jdbc:sqlite:database\\database.db";
 
